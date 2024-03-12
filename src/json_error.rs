fae981f6e1db892cd607df147a9a149e94846610
Guillaume <guillaume@centaurean.com>
2024-03-12 16:16:50 +0100

    Safer error

--- src/json_error.rs
+++ src/json_error.rs
@@ -1,24 +1,36 @@
 use std::fmt::{Display, Formatter, Result};
-use std::str::from_utf8_unchecked;
+use std::str::from_utf8;
+
+const SOURCE_SIZE: usize = 8192;
+const EXTRACT_PADDING: isize = 16;
 
 #[derive(Debug)]
 pub struct JsonError {
     pub index: usize,
-    pub extract: String,
+    pub extract: Option<String>,
+    pub source: Option<String>,
 }
 
 impl JsonError {
     pub fn new(bytes: &[u8], index: usize) -> Self {
-        let extract = &bytes[isize::max(0, index as isize - 8) as usize..usize::min(bytes.len(), index + 8)];
+        let source = from_utf8(&bytes[0..usize::min(bytes.len(), SOURCE_SIZE)]);
+        let extract = from_utf8(&bytes[isize::max(0, index as isize - EXTRACT_PADDING) as usize..usize::min(bytes.len(), index + EXTRACT_PADDING as usize)]);
         return JsonError {
             index,
-            extract: unsafe { from_utf8_unchecked(extract) }.to_owned(),
+            extract: match extract {
+                Ok(extract) => { Some(extract.to_owned()) }
+                Err(_) => { None }
+            },
+            source: match source {
+                Ok(source) => { if bytes.len() <= SOURCE_SIZE { Some(source.to_owned()) } else { Some(format!("{}...", source)) } }
+                Err(_) => { None }
+            },
         };
     }
 }
 
 impl Display for JsonError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "JSON error near '{}' : index {}", self.extract, self.index)
+        write!(f, "JSON error near '{}': index {} in {}", &self.extract.as_ref().unwrap_or(&String::from("?")), self.index, self.source.as_ref().unwrap_or(&String::from("?")))
     }
 }
