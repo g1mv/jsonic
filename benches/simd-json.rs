7c853c34717587c910fff8d3ef402e7fccc77e22
Guillaume <3772944+g1mv@users.noreply.github.com>
2025-03-19 20:35:26 +0100

    Mask code on unsupported platforms

--- benches/simd-json.rs
+++ benches/simd-json.rs
@@ -1,27 +1,28 @@
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+use divan::Bencher;
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+use serde_json::Value;
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 use std::fs::read_to_string;
 
-use divan::Bencher;
-use serde_json::Value;
-
 fn main() {
-    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
-        divan::main();
-    }
+    #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+    divan::main();
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 fn parse_file(bencher: Bencher, path: &str) {
-    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
-        let in_memory_json = read_to_string(path).unwrap();
+    let in_memory_json = read_to_string(path).unwrap();
 
-        let mut copy = in_memory_json.to_owned();
-        assert!(unsafe { simd_json::from_str::<Value>(&mut copy).is_ok() });
+    let mut copy = in_memory_json.to_owned();
+    assert!(unsafe { simd_json::from_str::<Value>(&mut copy).is_ok() });
 
-        bencher
-            .with_inputs(|| in_memory_json.to_owned())
-            .bench_local_values(move |mut copy| unsafe { simd_json::from_str::<Value>(&mut copy) });
-    }
+    bencher
+        .with_inputs(|| in_memory_json.to_owned())
+        .bench_local_values(move |mut copy| unsafe { simd_json::from_str::<Value>(&mut copy) });
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 #[divan::bench(args = ["./benches/data/canada.json", "./benches/data/citm_catalog.json", "./benches/data/twitter.json"])]
 fn parse(bencher: Bencher, path: &str) {
     parse_file(bencher, path);
--- benches/sonic-rs.rs
+++ benches/sonic-rs.rs
@@ -1,23 +1,24 @@
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+use divan::Bencher;
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+use serde_json::Value;
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 use std::fs::read_to_string;
 
-use divan::Bencher;
-use serde_json::Value;
-
 fn main() {
-    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
-        divan::main();
-    }
+    #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
+    divan::main();
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 fn parse_file(bencher: Bencher, path: &str) {
-    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
-        let in_memory_json = read_to_string(path).unwrap();
-        assert!(sonic_rs::from_str::<Value>(&in_memory_json).is_ok());
+    let in_memory_json = read_to_string(path).unwrap();
+    assert!(sonic_rs::from_str::<Value>(&in_memory_json).is_ok());
 
-        bencher.bench_local(move || sonic_rs::from_str::<Value>(&in_memory_json));
-    }
+    bencher.bench_local(move || sonic_rs::from_str::<Value>(&in_memory_json));
 }
 
+#[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 #[divan::bench(args = ["./benches/data/canada.json", "./benches/data/citm_catalog.json", "./benches/data/twitter.json"])]
 fn parse(bencher: Bencher, path: &str) {
     parse_file(bencher, path);
