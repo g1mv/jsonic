0a7eaee36819aa6c1a195bcaa7974ce6b87797f1
Guillaume <3772944+g1mv@users.noreply.github.com>
2025-03-19 19:59:01 +0100

    Restrict platform testing for simd-json

--- benches/simd-json.rs
+++ benches/simd-json.rs
@@ -4,20 +4,22 @@ use divan::Bencher;
 use serde_json::Value;
 
 fn main() {
-    divan::main();
+    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
+        divan::main();
+    }
 }
 
 fn parse_file(bencher: Bencher, path: &str) {
-    let in_memory_json = read_to_string(path).unwrap();
+    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
+        let in_memory_json = read_to_string(path).unwrap();
 
-    let mut copy = in_memory_json.to_owned();
-    assert!(unsafe { simd_json::from_str::<Value>(&mut copy).is_ok() });
+        let mut copy = in_memory_json.to_owned();
+        assert!(unsafe { simd_json::from_str::<Value>(&mut copy).is_ok() });
 
-    bencher.with_inputs(|| {
-        in_memory_json.to_owned()
-    }).bench_local_values(move |mut copy| unsafe {
-        simd_json::from_str::<Value>(&mut copy)
-    });
+        bencher
+            .with_inputs(|| in_memory_json.to_owned())
+            .bench_local_values(move |mut copy| unsafe { simd_json::from_str::<Value>(&mut copy) });
+    }
 }
 
 #[divan::bench(args = ["./benches/data/canada.json", "./benches/data/citm_catalog.json", "./benches/data/twitter.json"])]
