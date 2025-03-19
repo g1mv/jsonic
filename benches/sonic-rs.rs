d0a32be66f45b5fa9f8df19d58ce223c0c545dd8
Guillaume <3772944+g1mv@users.noreply.github.com>
2025-03-19 20:08:16 +0100

    Restrict platform testing for sonic-rs

--- benches/sonic-rs.rs
+++ benches/sonic-rs.rs
@@ -4,16 +4,18 @@ use divan::Bencher;
 use serde_json::Value;
 
 fn main() {
-    divan::main();
+    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
+        divan::main();
+    }
 }
 
 fn parse_file(bencher: Bencher, path: &str) {
-    let in_memory_json = read_to_string(path).unwrap();
-    assert!(sonic_rs::from_str::<Value>(&in_memory_json).is_ok());
+    if cfg!(any(target_arch = "x86_64", target_arch = "aarch64")) {
+        let in_memory_json = read_to_string(path).unwrap();
+        assert!(sonic_rs::from_str::<Value>(&in_memory_json).is_ok());
 
-    bencher.bench_local(move || {
-        sonic_rs::from_str::<Value>(&in_memory_json)
-    });
+        bencher.bench_local(move || sonic_rs::from_str::<Value>(&in_memory_json));
+    }
 }
 
 #[divan::bench(args = ["./benches/data/canada.json", "./benches/data/citm_catalog.json", "./benches/data/twitter.json"])]
